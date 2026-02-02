3824422e9cbb62ebf735bfac2d217ffdd35e04fb
GROHAAN <ag871998@gmail.com>
2026-02-02 13:54:53 +0530

    add function

--- project1/bin/project1.dart
+++ project1/bin/project1.dart
@@ -3,18 +3,18 @@ import 'package:project1/project1.dart' as project1;
 import 'dart:io';
 
 void main() {
-  print('Enter first number: '); // Use stdout.write to stay on the same line
-  int? a = stdin.readLineSync(); // Read input as a nullable string
+  print('Enter first number: '); // Prompt user to enter first number
+  String? a = stdin.readLineSync(); // Read first number from user input
+  print('Enter second number: '); // Prompt user to enter second number
+  String? b = stdin.readLineSync(); // Read second number from user input
+ 
 
-
-  print('Enter second number: '); // Use stdout.write to stay on the same line
-  int? b = stdin.readLineSync(); // Read input as a nullable string
 
   if (a != null && b != null) {
-    
-    print('multiple of $a and $b is ${project1.multiply(a, b)}');
+    String sum = a + b;
+    print('The sum of the numbers is: $sum');
   } else {
-    print('Input cannot be empty.');
+    print('Invalid input. Please enter two numbers.');
   }
 }
 
